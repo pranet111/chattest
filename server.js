780b85d0d4b1a2758487c2cc383f1032c404255b
pranet111 <63933768+pranet111@users.noreply.github.com>
2022-12-17 18:32:05 -0500

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-const routes = require('./routes');
-
-const server = http.createServer(routes.handleRequest);
-
-const port = 3000;
-server.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
